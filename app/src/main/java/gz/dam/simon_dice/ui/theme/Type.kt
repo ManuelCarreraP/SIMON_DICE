6621c58a0b72b71e54dd49b856a05888b383bff6
ManuelCarreraP <mcarrerapazo@danielcastelao.org>
2025-12-05 10:32:37 +0100

    Type Actualizado

--- app/src/main/java/gz/dam/simon_dice/ui/theme/Type.kt
+++ app/src/main/java/gz/dam/simon_dice/ui/theme/Type.kt
@@ -1,6 +1,5 @@
 package gz.dam.simon_dice.ui.theme
 
-
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -13,20 +12,6 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    ),
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
 )
