fe8648d72a9ceca15353ce98c2da8b3284de245a
ManuelCarreraP <mcarrerapazo@danielcastelao.org>
2025-12-05 10:33:04 +0100

    MainActivity Actualizado

--- app/src/main/java/gz/dam/simon_dice/MainActivity.kt
+++ app/src/main/java/gz/dam/simon_dice/MainActivity.kt
@@ -1,27 +1,37 @@
 package gz.dam.simon_dice
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import gz.dam.simon_dice.ui.theme.SimonDiceTheme
-
-
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             SimonDiceTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    SimonDiceUI()
+                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+                    // ViewModels
+                    val miViewModel: MiViewModel = androidx.lifecycle.viewmodel.compose.viewModel(
+                        factory = MiViewModelFactory(application)
+                    )
+                    val gameViewModel: VM = androidx.lifecycle.viewmodel.compose.viewModel()
+
+                    // Conectar ViewModels
+                    gameViewModel.setRecordViewModel(miViewModel)
+
+                    // UI sin modifier (usa padding interno en UI.kt)
+                    SimonDiceUI(
+                        gameViewModel = gameViewModel,
+                        miViewModel = miViewModel
+                    )
                 }
             }
         }
