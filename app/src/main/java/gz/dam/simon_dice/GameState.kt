d53beb9c9d5d9f3803f3aff3b31e7e52702ff943
ManuelCarreraP <mcarrerapazo@danielcastelao.org>
2025-12-05 10:33:34 +0100

    GameState Actualizado

--- app/src/main/java/gz/dam/simon_dice/GameState.kt
+++ app/src/main/java/gz/dam/simon_dice/GameState.kt
@@ -1,8 +1,8 @@
 package gz.dam.simon_dice
 
-
 /**
- * Clase sellada que representa todos los estados posibles del juego
+ * Clase sellada que representa todos los estados posibles del juego.
+ * Documentación: https://developer.android.com/kotlin/learn#sealed-classes
  */
 sealed class GameState {
     object Inicio : GameState()
